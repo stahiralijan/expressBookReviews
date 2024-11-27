7874d3de046bf234e7c0a11cb189ab5e1e8deb88
Tahir <stahiralijan@gmail.com>
2024-11-27 17:39:52 +0500

    Task 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,19 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  return ! (users.length > 0 && users.filter(user => user.username === username));
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  return users.filter(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(403).json({message: "Incorrect username or password"});
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,25 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
-  
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username) {
+    return res.status(403).json({message: "Username is required!"});
+  }
+
+  if(!password) {
+    return res.status(403).json({message: "Password is required!"});
+  }
+
+  if(isValid(username)) {
+    users.push({
+      username,
+      password
+    });
+    return res.status(200).json({message: "User registered! Proceed to login."});
+  }
+  return res.status(403).json({message: "User already registered!"});
 });
 
 // Get the book list available in the shop
