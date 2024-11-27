f2fec6860aa61fcd7a66d25eb3d40005d6ffc1bf
Tahir <stahiralijan@gmail.com>
2024-11-27 12:30:18 +0500

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  if(author) {
+    let keys = Object.keys(books);
+    for(let key of keys) {
+      
+      if(books[key].author == author) {
+        return res.status(200).json(books[key]);
+      }
+    }
+  }
+  return res.status(404).json({message: "Book not found!"});
 });
 
 // Get all books based on title
